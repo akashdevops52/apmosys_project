ce21a808f6d4ff8e1d2f0a6c30b290445a8bb10d
Akash <akashdevops52@gmail.com>
2026-03-02 13:27:39 +0530

    new akash

--- src/main/java/Basic/HelloWorld.java
+++ src/main/java/Basic/HelloWorld.java
@@ -5,5 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello There!!!");
 	}
-
+	public static void main(String[] args) {
+		System.out.println(" Akash!!!");
+	
 }
